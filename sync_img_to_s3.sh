6e012e67c199ebffe7db0f41101671b48e8e8602
alsaheem <adebisiayomide07@gmail.com>
2023-06-16 10:36:59 +0100

    update

--- sync_img_to_s3.sh
+++ sync_img_to_s3.sh
@@ -140,6 +140,8 @@ cd ..
 cd project27
 aws s3 sync images/ s3://darey-io-nonprod-pbl-projects/project27/
 
+cd ..
+
 cd practices
 aws s3 sync images/ s3://darey-io-nonprod-pbl-projects/practices/
 
